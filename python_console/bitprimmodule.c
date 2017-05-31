39dd23e00eaf427fe12bb30bc0c31c3addd0336b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-05-31 15:56:27 -0300

    initchain in python

--- python_console/bitprimmodule.c
+++ python_console/bitprimmodule.c
@@ -43,6 +43,17 @@ PyObject* module_executor_destruct(PyObject* self, PyObject* args) {
     return Py_BuildValue("");
 }
 
+static
+PyObject* module_executor_initchain(PyObject* self, PyObject* args) {
+    PyObject* py_exec;
+
+    if ( ! PyArg_ParseTuple(args, "O", &py_exec))
+        return NULL;
+
+    executor_t exec = (executor_t)PyCObject_AsVoidPtr(py_exec);
+    int res = executor_initchain(exec);
+    return Py_BuildValue("i", res);
+}
 
 
 static
@@ -50,6 +61,7 @@ PyMethodDef BitprimMethods[] = {
 
     {"construct",  module_executor_construct, METH_VARARGS, "Construct the executor object."},
     {"destruct",  module_executor_destruct, METH_VARARGS, "Destruct the executor object."},
+    {"initchain",  module_executor_initchain, METH_VARARGS, "Directory Initialization."},
 
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
---
+++ python_console/build_and_install.sh
@@ -0,0 +1,6 @@
+#
+
+# export LD_LIBRARY_PATH=/home/fernando/dev/bitprim/bitprim-node-cint/cmake-build-debug:$LD_LIBRARY_PATH
+
+python setup.py build
+sudo python setup.py install
