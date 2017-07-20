484d1018d163089b349f50f6d7864df3fa4faa20
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-07-20 14:33:44 -0300

    adding construction and serialization functions to header class

--- src/chain/header.cpp
+++ src/chain/header.cpp
@@ -22,58 +22,111 @@
 
 extern "C" {
 
-void header_destruct(header_t header) {
+
+//static header factory_from_data(const uint32_t version, const data_chunk& data);
+header_t chain_header_factory_from_data(uint32_t version, uint8_t* data, uint64_t n) {
+    libbitcoin::data_chunk data_cpp(data, data + n);
+    auto header = libbitcoin::message::header::factory_from_data(version, data_cpp);
+    return new libbitcoin::message::header(std::move(header)); //TODO: revisar todos los "new"'s que hay por todos lados para ver si podemos hacer un move de los recursos...
+}
+
+uint64_t /*size_t*/ chain_header_satoshi_fixed_size(uint32_t version) {
+    return libbitcoin::message::header::satoshi_fixed_size(version);
+}
+
+//data_chunk to_data(const uint32_t version) const;
+//void to_data(const uint32_t version, std::ostream& stream) const;
+//void to_data(const uint32_t version, writer& sink) const;
+uint8_t* chain_header_to_data(header_t header, uint32_t version) {
+    auto const& header_cpp = *static_cast<libbitcoin::message::header const*>(header);
+    auto data = header_cpp.to_data(version);
+
+    //Note: It is the responsability of the user to release/destruct the object
+    auto* ret = new libbitcoin::data_chunk(std::move(data));
+    return ret->data();
+}
+
+//void reset();
+void chain_header_reset(header_t header) {
+    return static_cast<libbitcoin::message::header*>(header)->reset();
+}
+
+//size_t serialized_size(const uint32_t version) const;
+uint64_t /*size_t*/ chain_header_serialized_size(header_t header, uint32_t version) {
+    return static_cast<libbitcoin::message::header const*>(header)->serialized_size(version);
+}
+
+//header();
+header_t chain_header_construct_default() {
+    return new libbitcoin::message::header();
+}
+
+//header(uint32_t version, const hash_digest& previous_block_hash, const hash_digest& merkle, uint32_t timestamp, uint32_t bits, uint32_t nonce);
+header_t chain_header_construct(uint32_t version, uint8_t* previous_block_hash, uint8_t* merkle, uint32_t timestamp, uint32_t bits, uint32_t nonce) {
+    //precondition: [previous_block_hash, 32) is a valid range
+    //              && [merkle, 32) is a valid range
+
+    libbitcoin::hash_digest previous_block_hash_cpp;
+    std::copy_n(previous_block_hash, previous_block_hash_cpp.size(), std::begin(previous_block_hash_cpp));
+
+    libbitcoin::hash_digest merkle_cpp;
+    std::copy_n(merkle, merkle_cpp.size(), std::begin(merkle_cpp));
+
+    return new libbitcoin::message::header(version, previous_block_hash_cpp, merkle_cpp, timestamp, bits, nonce);
+}
+
+void chain_header_destruct(header_t header) {
     auto header_cpp = static_cast<libbitcoin::message::header*>(header);
     delete header_cpp;
 }
 
-int header_is_valid(header_t header) {
+int chain_header_is_valid(header_t header) {
     return static_cast<libbitcoin::message::header const*>(header)->is_valid();
 }
 
-uint32_t header_version(header_t header) {
+uint32_t chain_header_version(header_t header) {
     return static_cast<libbitcoin::message::header const*>(header)->version();
 }
 
-void header_set_version(header_t header, uint32_t version) {
+void chain_header_set_version(header_t header, uint32_t version) {
     return static_cast<libbitcoin::message::header*>(header)->set_version(version);
 }
 
-uint32_t header_timestamp(header_t header) {
+uint32_t chain_header_timestamp(header_t header) {
     return static_cast<libbitcoin::message::header const*>(header)->timestamp();
 }
 
-void header_set_timestamp(header_t header, uint32_t timestamp) {
+void chain_header_set_timestamp(header_t header, uint32_t timestamp) {
     return static_cast<libbitcoin::message::header*>(header)->set_timestamp(timestamp);
 }
 
-uint32_t header_bits(header_t header) {
+uint32_t chain_header_bits(header_t header) {
     return static_cast<libbitcoin::message::header const*>(header)->bits();
 }
 
-void header_set_bits(header_t header, uint32_t bits) {
+void chain_header_set_bits(header_t header, uint32_t bits) {
     return static_cast<libbitcoin::message::header*>(header)->set_bits(bits);
 }
 
-uint32_t header_nonce(header_t header) {
+uint32_t chain_header_nonce(header_t header) {
     return static_cast<libbitcoin::message::header const*>(header)->nonce();
 }
 
-void header_set_nonce(header_t header, uint32_t nonce) {
+void chain_header_set_nonce(header_t header, uint32_t nonce) {
     return static_cast<libbitcoin::message::header*>(header)->set_nonce(nonce);
 }
 
-hash_t header_previous_block_hash(header_t header) {
+hash_t chain_header_previous_block_hash(header_t header) {
     auto const& hash_cpp = static_cast<libbitcoin::message::header const*>(header)->previous_block_hash();
     return hash_cpp.data();
 }
 
-hash_t header_merkle(header_t header) {
+hash_t chain_header_merkle(header_t header) {
     auto const& hash_cpp = static_cast<libbitcoin::message::header const*>(header)->merkle();
     return hash_cpp.data();
 }
 
-hash_t header_hash(header_t header) {
+hash_t chain_header_hash(header_t header) {
     auto const& hash_cpp = static_cast<libbitcoin::message::header const*>(header)->hash();
     return hash_cpp.data(); //TODO: returning a dangling pointer
 }
@@ -95,9 +148,6 @@ hash_t header_hash(header_t header) {
 //
 //// Properties (size, accessors, cache).
 ////-----------------------------------------------------------------------------
-//
-//static size_t satoshi_fixed_size();
-//size_t serialized_size() const;
 //
 //
 //void set_previous_block_hash(const hash_digest& value);
