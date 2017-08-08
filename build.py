62c2bb801a6fd90415414aa6071e0a29792415c8
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-08 20:10:42 -0300

    adding static lib

--- build.py
+++ build.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
+        # if settings["build_type"] == "Release" \
+        #         and settings["arch"] == "x86_64" \
+        #         and options["bitprim-node-cint:shared"] == True:
         if settings["build_type"] == "Release" \
-                and settings["arch"] == "x86_64" \
-                and options["bitprim-node-cint:shared"] == True:
+                and settings["arch"] == "x86_64":
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
