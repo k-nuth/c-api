a197e9a97902dfb49c0f1cd4efab772f9f3ce5f0
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-19 15:35:14 -0300

    MD build

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and not options["bitprim-node-cint:shared"] \
-                and not ("compiler.runtime" in settings and settings["compiler.runtime"] == "MD"):
+                and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
