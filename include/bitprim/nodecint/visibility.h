4f8c516e10bae11d5d223e128be2a8245f0d82cf
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-21 18:18:39 -0300

    change bintray user and api key

--- include/bitprim/nodecint/visibility.h
+++ include/bitprim/nodecint/visibility.h
@@ -20,26 +20,34 @@
 #ifndef BITPRIM_NODECINT_VISIBILITY_H_
 #define BITPRIM_NODECINT_VISIBILITY_H_
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-  #ifdef bitprim_EXPORTS
-    #ifdef __GNUC__
-      #define BITPRIM_EXPORT __attribute__ ((dllexport))
-    #else
-      #define BITPRIM_EXPORT __declspec(dllexport)
-    #endif
-  #else
-    #ifdef __GNUC__
-      #define BITPRIM_EXPORT __attribute__ ((dllimport))
-    #else
-      #define BITPRIM_EXPORT __declspec(dllimport)
-    #endif
-  #endif
-#else
-  #if __GNUC__ >= 4
-    #define BITPRIM_EXPORT __attribute__ ((visibility ("default")))
-  #else
+
+#if defined(BITPRIM_LIB_STATIC)
     #define BITPRIM_EXPORT
-  #endif
+#else
+    #if defined(_WIN32) || defined(__CYGWIN__)
+      #ifdef bitprim_EXPORTS
+        #ifdef __GNUC__
+          #define BITPRIM_EXPORT __attribute__ ((dllexport))
+        #else
+          #define BITPRIM_EXPORT __declspec(dllexport)
+        #endif
+      #else
+        #ifdef __GNUC__
+          #define BITPRIM_EXPORT __attribute__ ((dllimport))
+        #else
+          #define BITPRIM_EXPORT __declspec(dllimport)
+        #endif
+      #endif
+    #else
+      #if __GNUC__ >= 4
+        #define BITPRIM_EXPORT __attribute__ ((visibility ("default")))
+      #else
+        #define BITPRIM_EXPORT
+      #endif
+    #endif
 #endif
+
+
+
 
 #endif /* BITPRIM_NODECINT_VISIBILITY_H_ */
