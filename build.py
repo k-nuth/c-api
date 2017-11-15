96684283c978b850e91ac8d488dcd40ced62bb48
Dario Ramos <dario.fenix@gmail.com>
2017-11-15 14:47:24 -0300

    T57: Shared build for Windows (needed for bitprim-cs) (#3)
    * T57: Add shared:true builds for Windows
    
    * T57: Test
    
    * T57: Remove test code
    
    * T57: Test

--- build.py
+++ build.py
@@ -6,12 +6,10 @@ if __name__ == "__main__":
                                  remotes="https://api.bintray.com/conan/bitprim/bitprim",
                                  archs=["x86_64"])
 
-    builder.add_common_builds(shared_option_name="bitprim-node-cint:shared")
-
+    builder.add_common_builds(shared_option_name="bitprim-node-cint:shared")    
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
-                and not options["bitprim-node-cint:shared"] \
                 and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
 
             env_vars["BITPRIM_BUILD_NUMBER"] = os.getenv('BITPRIM_BUILD_NUMBER', '-')
