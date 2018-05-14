54cf0491e9ab018d959924001aa6b1763d35c2b1
dario-ramos <dario.fenix@gmail.com>
2018-05-14 12:23:58 -0300

    [BLCK-72] fetch_xtns wrapper

--- src/chain/chain.cpp
+++ src/chain/chain.cpp
@@ -642,6 +642,15 @@ error_code_t chain_get_history(chain_t chain, payment_address_t address, uint64_
     return res;
 }
 
+void chain_fetch_txns(chain_t chain, void* ctx, payment_address_t address, uint64_t max, uint64_t start_height, transactions_by_addres_fetch_handler_t handler){
+    libbitcoin::wallet::payment_address const& address_cpp = *static_cast<const libbitcoin::wallet::payment_address*>(address);
+
+    safe_chain(chain).fetch_txns(address_cpp, max, start_height, [chain, ctx, handler](std::error_code const& ec, libbitcoin::chain::hash_digest::list txs) {
+        auto new_txs = new libbitcoin::chain::hash_digest::list(txs);
+        handler(chain, ctx, static_cast<error_code_t>(ec.value()), new_txs);
+    });
+}
+
 void chain_fetch_stealth(chain_t chain, void* ctx, binary_t filter, uint64_t from_height, stealth_fetch_handler_t handler) {
 	auto* filter_cpp_ptr = static_cast<const libbitcoin::binary*>(filter);
 	libbitcoin::binary const& filter_cpp  = *filter_cpp_ptr;
