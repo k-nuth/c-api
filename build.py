7bb2a385db2c0fc87312384336ed3024853c5ecf
Mario <the4power@yahoo.com.ar>
2018-12-20 15:40:11 -0300

    fix

--- build.py
+++ build.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
                 if ci_currency == "BCH" and not with_keoken:
                     opts_bch_new = copy.deepcopy(options)
                     opts_bch_new["%s:use_domain" % name] = "True"
-                    opts_bch_new["%s:db" % name] = "new"
+                    opts_bch_new["%s:db" % name] = "default"
                     handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_bch_new, env_vars, build_requires)
 
                 handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, options, env_vars, build_requires)
