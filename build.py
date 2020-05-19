a0e78db1b603a4c9621d4c43ced8870201b99464
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-19 11:28:33 -0300

    disable BTC and BCH full builds on CI

--- build.py
+++ build.py
@@ -24,22 +24,22 @@ if __name__ == "__main__":
                 options["%s:tests" % name] = "True"
 
             opts_bch = copy.deepcopy(options)
-            opts_btc = copy.deepcopy(options)
+            # opts_btc = copy.deepcopy(options)
 
             opts_bch["%s:currency" % name] = "BCH"
-            opts_btc["%s:currency" % name] = "BTC"
+            # opts_btc["%s:currency" % name] = "BTC"
 
-            opts_bch_full = copy.deepcopy(opts_bch)
-            opts_bch_full["%s:db" % name] = "full"
+            # opts_bch_full = copy.deepcopy(opts_bch)
+            # opts_bch_full["%s:db" % name] = "full"
 
             # handle_microarchs("*:march_id", march_ids, filtered_builds, settings, opts_bch_full, env_vars, build_requires)
             # handle_microarchs("*:march_id", march_ids, filtered_builds, settings, opts_bch, env_vars, build_requires)
             # handle_microarchs("*:march_id", march_ids, filtered_builds, settings, opts_btc, env_vars, build_requires)
             # filter_marchs_tests(name, filtered_builds, ["%s:tests" % name], "*:march_id")
 
-            handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_bch_full, env_vars, build_requires)
+            # handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_bch_full, env_vars, build_requires)
             handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_bch, env_vars, build_requires)
-            handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_btc, env_vars, build_requires)
+            # handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_btc, env_vars, build_requires)
             filter_marchs_tests(name, filtered_builds, ["%s:tests" % name])
 
     builder.builds = filtered_builds
