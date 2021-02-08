1b1afec60a090650a900c9372ab425a6b1c903b5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-08 15:50:12 -0300

    adds microarchitecture_id to libconfig example

--- console/test_libconfig.cpp
+++ console/test_libconfig.cpp
@@ -10,13 +10,14 @@ int main(int argc, char* argv[]) {
 
     auto config = kth_libconfig_get();
 
-    std::cout << "log_library:       " << config.log_library << '\n';
-    std::cout << "use_libmdbx:       " << config.use_libmdbx << '\n';
-    std::cout << "version:           " << config.version << '\n';
-    std::cout << "microarchitecture: " << config.microarchitecture << '\n';
-    std::cout << "currency:          " << config.currency << '\n';
-    std::cout << "mempool:           " << config.mempool << '\n';
-    std::cout << "db_mode:           " << config.db_mode << '\n';
-    std::cout << "db_readonly:       " << config.db_readonly << '\n';
-    std::cout << "debug_mode:        " << config.debug_mode << '\n';
+    std::cout << "log_library:          " << config.log_library << '\n';
+    std::cout << "use_libmdbx:          " << config.use_libmdbx << '\n';
+    std::cout << "version:              " << config.version << '\n';
+    std::cout << "microarchitecture:    " << config.microarchitecture << '\n';
+    std::cout << "microarchitecture_id: " << config.microarchitecture_id << '\n';
+    std::cout << "currency:             " << config.currency << '\n';
+    std::cout << "mempool:              " << config.mempool << '\n';
+    std::cout << "db_mode:              " << config.db_mode << '\n';
+    std::cout << "db_readonly:          " << config.db_readonly << '\n';
+    std::cout << "debug_mode:           " << config.debug_mode << '\n';
 }
